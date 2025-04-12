5531e589a09cb90cda3faae25ef5b1acc660cf95
Sebastian Jean-Francois <sebeld9@gmail.com>
2025-04-12 19:30:18 -0400

    Directly creating point object in write parameter

--- test/proxy/database.cpp
+++ test/proxy/database.cpp
@@ -10,12 +10,91 @@ void writeRadioTelemetry(const Telemetry &t, std::unique_ptr<influxdb::InfluxDB>
 {
     std::cout << "Writing Telemetry data... " << std::endl;
 
-    auto point = influxdb::Point{"Ground Radio"};
-    initPoint(point, t);
+    // auto point = influxdb::Point{"Fill Radio"};
+    // initPoint(point, t);
+    const std::string title = "Ground Radio";
 
-
+    int event_i = 0;
+    int meta_i = 0;
     // Write the point to the database
-    // influxdb->write(std::move(point));
+    influxdb->write( influxdb::Point{title}
+    .addField("metadata", t.metadata)
+    .addField("ms_since_boot", t.ms_since_boot)
+    .addField("events", t.events)
+    .addField("altimeter_altitude", t.altitude)
+    .addField("altimeter_temperature", t.temperature)
+    .addField("gps_latitude", t.gps_latitude)
+    .addField("gps_longitude", t.gps_longitude)
+    .addField("gps_satellites_in_view", t.gps_num_satellites)
+    .addField("gps_unix_time", t.unix_time)
+    .addField("gps_horizontal_accuracy", t.horizontal_accuracy)
+    .addField("imu_acceleration_x", t.imu_accel_x)
+    .addField("imu_acceleration_y", t.imu_accel_y)
+    .addField("imu_acceleration_z", t.imu_accel_z)
+    .addField("imu_gyro_x", t.imu_gyro_x)
+    .addField("imu_gyro_y", t.imu_gyro_y)
+    .addField("imu_gyro_z", t.imu_gyro_z)
+    .addField("imu_orientation_x", t.imu_orientation_x)
+    .addField("imu_orientation_y", t.imu_orientation_y)
+    .addField("imu_orientation_z", t.imu_orientation_z)
+    .addField("accelerometer_acceleration_x", t.accel_x)
+    .addField("accelerometer_acceleration_y", t.accel_y)
+    .addField("accelerometer_acceleration_z", t.accel_z)
+    .addField("battery_volt", t.battery_volt)
+    .addField("pressure_pt3", t.pressure_pt3)
+    .addField("pressure_pt4", t.pressure_pt4)
+    .addField("rtd_temperature", t.rtd_temperature)
+    .addField("blims_motor_state", t.motor_position)
+  
+    // Add the events to the point
+    .addField("altitude_armed", getEvent(t, event_i++))
+    .addField("altimeter_init_failed", getEvent(t, event_i++))
+    .addField("altimeter_reading_failed", getEvent(t, event_i++)) 
+    .addField("gps_init_failed", getEvent(t, event_i++))
+    .addField("gps_reading_failed", getEvent(t, event_i++)) 
+    .addField("imu_init_failed", getEvent(t, event_i++)) 
+    .addField("imu_reading_failed", getEvent(t, event_i++)) 
+    .addField("accelerometer_init_failed", getEvent(t, event_i++)) 
+    .addField("accelerometer_reading_failed", getEvent(t, event_i++)) 
+    .addField("adc_init_failed", getEvent(t, event_i++)) 
+    .addField("adc_reading_failed", getEvent(t, event_i++)) 
+    .addField("fram_init_failed", getEvent(t, event_i++)) 
+    .addField("fram_read_failed", getEvent(t, event_i++)) 
+    .addField("fram_write_failed", getEvent(t, event_i++)) 
+    .addField("sd_init_failed", getEvent(t, event_i++)) 
+    .addField("sd_write_failed", getEvent(t, event_i++)) 
+    .addField("mav_was_actuated", getEvent(t, event_i++)) 
+    .addField("sv_was_actuated", getEvent(t, event_i++)) 
+    .addField("main_deploy_wait_end", getEvent(t, event_i++)) 
+    .addField("main_log_shutoff", getEvent(t, event_i++)) 
+    .addField("cycle_overflow", getEvent(t, event_i++)) 
+    .addField("unknown_command_received", getEvent(t, event_i++)) 
+    .addField("launch_command_received", getEvent(t, event_i++)) 
+    .addField("mav_command_received", getEvent(t, event_i++)) 
+    .addField("sv_command_received", getEvent(t, event_i++)) 
+    .addField("safe_command_received", getEvent(t, event_i++)) 
+    .addField("reset_card_command_received", getEvent(t, event_i++)) 
+    .addField("reset_fram_command_received", getEvent(t, event_i++)) 
+    .addField("state_change_command_received", getEvent(t, event_i++)) 
+    .addField("umbilical_disconnected", getEvent(t, event_i++))
+
+    // Add metadata to the point
+    .addField("alt_armed", getMetadata(t, meta_i++))
+    .addField("alt_valid", getMetadata(t, meta_i++))
+    .addField("gps_valid", getMetadata(t, meta_i++))
+    .addField("imu_valid", getMetadata(t, meta_i++))
+    .addField("acc_valid", getMetadata(t, meta_i++))
+    .addField("umbilical_connection", getMetadata(t, meta_i++))
+    .addField("adc_valid", getMetadata(t, meta_i++))
+    .addField("fram_valid", getMetadata(t, meta_i++))
+    .addField("sd_valid", getMetadata(t, meta_i++))
+    .addField("gps_msg_fresh", getMetadata(t, meta_i++))
+    .addField("rocket_was_safed", getMetadata(t, meta_i++))
+    .addField("mav_state", getMetadata(t, meta_i++))
+    .addField("sv2_state", getMetadata(t, meta_i++))
+    .addField("flight_mode", getFlightState(t))
+
+    .setTimestamp(std::chrono::system_clock::now()));
 
     std::cout<<"Data successfully written\n\n";
 }
